a6d54e9568107fb1cf1a9b756e639867011f4565
Eric Zoerner <eric.zoerner@gmail.com>
2023-03-28 08:14:29 -0700

    Add Chapter02 haddock and update Chapter01 haddock

--- src/Chapter01.hs
+++ src/Chapter01.hs
@@ -1,24 +1,31 @@
 {- |
 Module      : Chapter01
 Description : Calculating with Haskell
+
 Copyright   : (c) Eric Zoerner, 2023
 License     : BSD3
 Maintainer  : eric.zoerner@proton.me
+-}
 
-= Exercise 1.1.
-/Evaluate sin 30 in GHCi. Why is it not equal to 0.5?/
+module Chapter01 where
+
+-- * Exercise 1.1.
+{- $ex11
+Evaluate @sin 30@ in GHCi. Why is it not equal to @0.5@?
 
 >>> sin 30
 -0.9880316240928618
 
-Because `sin` expects the angle in radians.
+/Because @sin@ expects the angle in radians./
 
 >>> sin (30/180 * pi)
 0.49999999999999994
+-}
 
-= Exercise 1.2.
-/Add parentheses to the following expressions to indicate the order in which
-Haskell’s precedence and associativity rules would evaluate the expressions:/
+-- * Exercise 1.2.
+{- $ex12
+Add parentheses to the following expressions to indicate the order in which
+Haskell’s precedence and associativity rules would evaluate the expressions:
 
 @
   (a) 2 ^ 3 ^ 4
@@ -27,7 +34,7 @@ Haskell’s precedence and associativity rules would evaluate the expressions:/
   (d) log 49 \/ 7
 @
 
-__Solutions:__
+=== Solutions:
 
 @
   (a) 2 ^ (3 ^ 4)
@@ -35,15 +42,20 @@ __Solutions:__
   (c) 7 - (5 \/ 4)
   (d) (log 49) \/ 7
 @
+-}
 
-= Exercise 1.3.
-/Use GHCi to find @log₂ 32.@/
+-- * Exercise 1.3.
+{- $ex13
+Use GHCi to find @log₂ 32.@
 
 >>> logBase 2 32
 5.0
+-}
 
-= Exercise 1.4.
-/Use the @atan2@ function in GHCi to find the polar coordinates (r,θ) associated with Cartesian coordinates (x, y) = (–3,4)./
+-- * Exercise 1.4.
+{- $ex14
+Use the @atan2@ function in GHCi to find the polar coordinates @(r,θ)@ associated
+with Cartesian coordinates @(x, y) = (–3,4)@.
 
 >>> theta = atan2 4 (-3)
 >>> x = -3
@@ -52,20 +64,26 @@ __Solutions:__
 >>> r = sqrt (x**2 + y**2)
 >>> (r, theta)
 (5.0,2.214297435588181)
+-}
 
-= Exercise 1.5.
-/Find a new example of a calculation in which the computer produces a result that is just a little bit different from the exact result./
+-- * Exercise 1.5.
+{- $ex15
+Find a new example of a calculation in which the computer produces a result that
+is just a little bit different from the exact result.
 
 >>> 3 ** (logBase 3 10)
 10.000000000000002
+-}
 
-= Exercise 1.6.
-/Why is there no associativity listed for the equality, inequality, and comparison operators in Table 1-2? (Hint: write down the simplest expression you can think of that would require the associativity rules to resolve the precedence of comparison operators and try to make sense of it.)/
+-- * Exercise 1.6.
+{- $ex16
+Why is there no associativity listed for the equality, inequality, and
+comparison operators in Table 1-2? (Hint: write down the simplest expression you
+can think of that would require the associativity rules to resolve the
+precedence of comparison operators and try to make sense of it.)
 
 > 3 < 4 < 5
 
-Because comparison, equality, and inequality operators evaluate to Boolean
-values which cannot be further operated on with these same operators.
- -}
+/Because comparison, equality, and inequality operators evaluate to Boolean values which cannot be further operated on with these same operators./
+-}
 
-module Chapter01 where
--- src/Chapter02.hs
+++ src/Chapter02.hs
@@ -1,55 +1,101 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Chapter02
+-- Description : Writing Basic Functions
+--
+-- Copyright   : (c) Eric Zoerner, 2023
+-- License     : BSD3
+-- Maintainer  : eric.zoerner@proton.me
+--
+-----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module Chapter02 where
 
----------------
--- Exercise 2.1
----------------
+-- * Exercise 2.1
+
+-- | @f(x) = √ (1 + x)@
+--
+-- >>> f 0
+-- 1.0
+-- >>> f 1
+-- 1.4142135623730951
+-- >>> f 3
+-- 2.0
 f :: Floating a => a -> a
 f x = sqrt (1 + x)
 
----------------
--- Exercise 2.2
----------------
+-- * Exercise 2.2.
+
+-- | Standard Earth gravity acceleration at sea level in m/s².
 stdGravityMPS2 :: Double
 stdGravityMPS2 = 9.80665
 
+-- | Assuming an initial velocity of 30 m\/s, accepts as input the time
+-- (after the rock was thrown) in seconds and gives
+-- as output the height of the rock in meters.
 yRock30 :: Double -> Double
 yRock30 t = 0.5 * (-stdGravityMPS2) * t ** 2 + 30 * t
 
----------------
--- Exercise 2.3
----------------
+-- * Exercise 2.3.
+
+-- | Accepts as input the time (after the rock was thrown) in seconds and
+-- gives as output the upward velocity of the rock in meters per second.
+-- A downward velocity is returned as a negative number.
 vRock30 :: Double -> Double
 vRock30 t = 30 - stdGravityMPS2 * t
 
----------------
--- Exercise 2.4
----------------
+-- * Exercise 2.4.
+
+-- | Computes the sine of an angle given in degrees.
+--
+-- >>> sinDeg 30
+-- 0.49999999999999994
 sinDeg :: Double -> Double
 sinDeg deg = sin (deg * pi / 180)
 
----------------
--- Exercise 2.5
----------------
-cubeRoot :: Double -> Double
-cubeRoot x = x ** (1 / 3)
+-- * Exercise 2.5.
 
+-- | @f(x) = ∛x@
+f' :: Double -> Double
+f' x = x ** (1 / 3)
+
+-- | @g(ɣ) = e^ɣ+8^ɣ@
 g :: Double -> Double
-g y = exp y + 8 ** y
+g ɣ = exp ɣ + 8 ** ɣ
 
+-- | @h(x) = 1/√((x-5)²+16)@
 h :: Double -> Double
-h x = 1 / ((x - 5) ** 2 + 16)
+h x = 1 / sqrt ((x - 5) ** 2 + 16)
 
-gamma :: Double -> Double
-gamma beta = 1 / sqrt (1 - beta ** 2)
+-- | @ɣ(β) = 1/√(1 - β²)@
+ɣ :: Double -> Double
+ɣ β = 1 / sqrt (1 - β ** 2)
 
+-- | @U(x) = 1/(10+x)+1(10-x)@
 bigU :: Double -> Double
 bigU x = (1 / (10 - x)) + (1 / (10 - x))
 
+-- | @L(l) = √(l(l + 1))@
 bigL :: Double -> Double
-bigL ell = sqrt (ell * (ell + 1))
+bigL l = sqrt (l * (l + 1))
 
+-- | @E(x) = 1 / (|x|³)@
 bigEx :: Double -> Double
 bigEx x = 1 / abs x ** 3
 
+-- | @E(z) = 1 \/ (z²+4)^(3\/2)@
 bigEz :: Double -> Double
 bigEz z = 1 / ((z ** 2 + 4) ** (3 / 2))
+
+-- * Exercise 2.6.
+-- $ex26
+--
+-- (a) @\b -> 1 / sqrt (1 - b ** 2)@
+--
+-- (b)
+--
+-- >>> (\b -> 1 / sqrt (1 - b ** 2)) 0.8
+-- 1.666666666666667
+
